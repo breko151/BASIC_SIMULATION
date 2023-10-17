db618064f71a56a37e2d5fea22be61a045d7a11e
breko151 <brekopablo@gmail.com>
2023-10-16 19:35:00 -0600

    Variance test

--- simulation/test_goodness.py
+++ simulation/test_goodness.py
@@ -4,11 +4,11 @@
 # Date:
 #   16/10/2023
 
-# Import libraries needed
-from . import norm
+# Import libraries needed.
+from . import norm, chi2
 from . import np
 
-# Mean and distance tests
+# Mean and distance tests.
 def mean_test(numbers, alpha=0.05):
     """
         Mean test.
@@ -19,7 +19,7 @@ def mean_test(numbers, alpha=0.05):
             test: a boolean value.
     """
     # Get sample mean.
-    sample_mean = np.mean(numbers)
+    sample_mean = numbers.mean(ddof=1)
     # Get z.
     z_alpha_over_2 = norm.ppf(1 - alpha / 2)
     # Get limits.
@@ -28,6 +28,32 @@ def mean_test(numbers, alpha=0.05):
     # Try test.
     test = lower_limit <= sample_mean <= upper_limit
     return test
+
+
+# Variance test.
+def variance_test(numbers, alpha=0.05):
+    """
+        Variance test.
+        Arguments:
+            numbers: a list of values.
+            alpha: a float value.
+        Returns:
+            test: a boolean value.
+    """
+    # Get sample var.
+    var = numbers.var(ddof=1)
+    # Get Deegres of freedom.
+    df = len(numbers)
+    # Get Chi^2.
+    chi_2 = chi2.ppf(1 - alpha / 2, df - 1)
+    chi_1 = chi2.ppf(alpha / 2, df - 1)
+    # Get limits.
+    lim_inf = chi_1 / (12 * (df - 1))
+    lim_sup = chi_2 / (12 * (df - 1))
+    # Try test.
+    test = var >= lim_inf and var <= lim_sup
+    return test
+
 
 
 # Identificación  de patrones
